697bf830d6e00fc10e7f27121c757d6748da7947
Chansol Lim <email@address.com>
2021-06-21 12:16:37 +0900

    add condition

--- main/back/flask/app.py
+++ main/back/flask/app.py
@@ -36,4 +36,6 @@ class ItemList(Resource):
 
 api.add_resource(Item, '/item/<string:name>')
 
-app.run(host="127.0.0.1", port=5000, debug=True)
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5000, debug=True)
