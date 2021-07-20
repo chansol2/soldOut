a4ea1d3beeae0a6ff4b24a3bab25672e36927c71
Chansol Lim <email@address.com>
2021-07-20 13:43:20 +0900

    edit index resource

--- main/back/flask/app/resources/index.py
+++ main/back/flask/app/resources/index.py
@@ -7,11 +7,14 @@
 # from app.models.oneRoom import OneRoomModel
 # from app.models.ssg import SSGModel
 # from app.models.tenByTen import TenByTenModel
+from app.models.products import ProductModel
 from flask_restful import Resource
 from app import db
+from app import app
 
 
 class Index(Resource):
     def get(self):
         app.logger.info("index:here")
-        return 200
+        ssg = ProductModel.query.filter_by(seller_id=15).first().json()
+        return ssg, 200
